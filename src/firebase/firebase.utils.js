bb9bf3c2459fd10ffc11a6339ad3a5f6add10e22
Guilherme Parente <guilherme.parente17@outlook.com>
2022-01-23 14:59:30 -0300

    implemented firebase utils, included ability to share authenticated users into our firestore databse

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,18 +1,43 @@
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const config = {
-    apiKey: "AIzaSyDndQ1H9gPaBy0w70kBSvSC4HFgsOVPdVw",
-    authDomain: "crwn-db-5df4a.firebaseapp.com",
-    projectId: "crwn-db-5df4a",
-    storageBucket: "crwn-db-5df4a.appspot.com",
-    messagingSenderId: "154366023543",
-    appId: "1:154366023543:web:07701774f402cf3cdff372",
-    measurementId: "G-16HGHFNGJF"
-  }
+  apiKey: "AIzaSyDndQ1H9gPaBy0w70kBSvSC4HFgsOVPdVw",
+  authDomain: "crwn-db-5df4a.firebaseapp.com",
+  projectId: "crwn-db-5df4a",
+  storageBucket: "crwn-db-5df4a.appspot.com",
+  messagingSenderId: "154366023543",
+  appId: "1:154366023543:web:07701774f402cf3cdff372",
+  measurementId: "G-16HGHFNGJF"
+};
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
